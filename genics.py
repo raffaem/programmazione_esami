3c01adea04780198106698e0d0df9d91ab4492c7
Raffaele Mancuso <raffaele.mancuso4@unibo.it>
2024-07-12 16:35:35 +0200

    Make tables

--- genics.py
+++ genics.py
@@ -54,22 +54,22 @@ for index, row in df.iterrows():
     #
     n = row["n"]
     print(f"Processing exam {n}")
-    mymd.write(f"**Appello n. {n}** \\\n")
+    # mymd.write(f"**Appello n. {n}** \n\n")
+    table = pd.DataFrame()
     #
     e = Event()
     e.name = f"[{course}] Appello {n} - Inizio Iscrizioni"
     e.begin = rome2utc(row["iscrizioni_inizio"])
     e.make_all_day()
     c.events.add(e)
-    mymd.write(f"Inizio iscrizioni: {
-               timestamp(row['iscrizioni_inizio'])} \\\n")
+    table["Inizio iscrizioni"] = [timestamp(row['iscrizioni_inizio'])]
     #
     e = Event()
     e.name = f"[{course}] Appello {n} - Fine Iscrizioni"
     e.begin = rome2utc(row["iscrizioni_fine"])
     e.make_all_day()
     c.events.add(e)
-    mymd.write(f"Fine iscrizioni: {timestamp(row['iscrizioni_fine'])} \\\n")
+    table["Fine iscrizioni"] = [timestamp(row['iscrizioni_fine'])]
     #
     e = Event()
     e.name = f"[{course}] Appello {n} - Esame"
@@ -77,35 +77,39 @@ for index, row in df.iterrows():
     e.end = rome2utc(row["esame_fine"])
     e.location = str(row["esame_aula"])
     c.events.add(e)
-    mymd.write(f"Esame: {timestamp(row['esame_inizio'])} {
-               str(row['esame_aula'])} \\\n")
+    table["Esame"] = [timestamp(row['esame_inizio']) +
+                      " " + str(row['esame_aula'])]
     #
     e = Event()
     e.name = f"[{course}] Appello {n} - Pubblicazione"
     e.begin = rome2utc(row["pubblicazione"])
     e.make_all_day()
     c.events.add(e)
-    mymd.write(f"Pubblicazione: {dateonly(row['pubblicazione'])} \\\n")
+    table["Pubblicazione"] = [dateonly(row['pubblicazione'])]
     #
     e = Event()
     e.name = f"[{course}] Appello {n} - Visione"
     e.begin = rome2utc(row["visione_inizio"])
     e.end = rome2utc(row["visione_fine"])
-    e.location = str(row["visione_link"])
     c.events.add(e)
-    mymd.write(f"Visione: {timestamp(row['visione_inizio'])}")
+    table["Visione"] = [timestamp(row['visione_inizio'])]
     #
     e = Event()
     e.name = f"[{course}] Appello {n} - Verbalizzazione"
     e.begin = rome2utc(row["verbalizzazione"])
     e.make_all_day()
     c.events.add(e)
-    mymd.write(f"Verbalizzazione: {dateonly(row['verbalizzazione'])} \n")
+    table["Verbalizzazione"] = dateonly(row['verbalizzazione'])
     #
-    mymd.write("\n")
+    table = table.transpose()
+    table.index.name = f"Appello n. {n}"
+    table.columns = [""]
+    mymd.write(table.to_markdown())
+    mymd.write("\n\n")
 
 print(c.events)
 with open(outdir/f'{course}.ics', 'w') as my_file:
     my_file.writelines(c.serialize_iter())
 
 mymd.close()
+
